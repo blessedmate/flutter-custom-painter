b8f81f9359833d1e1d2936d0ed1c5c789fd2bf7c
Mate <md.quintero@uniandes.edu.co>
2023-06-19 17:10:23 -0500

    square rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:custom_painter/src/pages/headers_page.dart';
+import 'package:custom_painter/src/pages/animations_page.dart';
+// import 'package:custom_painter/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HeadersPage(),
+      home: const AnimationsPage(),
     );
   }
 }
---
+++ lib/src/pages/animations_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimationsPage extends StatelessWidget {
+  const AnimationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: AnimatedSquare(),
+      ),
+    );
+  }
+}
+
+class AnimatedSquare extends StatefulWidget {
+  const AnimatedSquare({
+    super.key,
+  });
+
+  @override
+  State<AnimatedSquare> createState() => _AnimatedSquareState();
+}
+
+class _AnimatedSquareState extends State<AnimatedSquare>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 4000,
+      ),
+    );
+
+    rotation = Tween(begin: 0.0, end: 2 * math.pi).animate(controller);
+
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.forward();
+    return AnimatedBuilder(
+      animation: controller,
+      // child: const _Square(),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(
+          angle: rotation.value,
+          child: const _Square(),
+        );
+      },
+    );
+  }
+}
+
+class _Square extends StatelessWidget {
+  const _Square({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(color: Colors.blue),
+    );
+  }
+}
